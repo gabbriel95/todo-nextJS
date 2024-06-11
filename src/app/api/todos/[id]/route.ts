af4c33566b77d96ab3318f6817e2eef1aedf10a8
Gabriel <juanmendez0013@gmail.com>
2024-06-11 15:36:21 -0300

    se agrego el put

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -19,3 +19,23 @@ export async function GET(request: Request, { params }: Segments) {
 
   return NextResponse.json(todo);
 }
+
+export async function PUT(request: Request, { params }: Segments) {
+  const todo = await prisma.todo.findUnique({ where: { id: params.id } });
+
+  if (!todo) {
+    return NextResponse.json(
+      { message: "No existe un Todo con ese ID" },
+      { status: 404 }
+    );
+  }
+
+  const body = await request.json();
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id: params.id },
+    data: body,
+  });
+
+  return NextResponse.json(updatedTodo);
+}

fa088e7a443db89388a70f42d79b801f0a6a932c
Gabriel <juanmendez0013@gmail.com>
2024-06-12 11:19:40 -0300

    crud todos completed

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,29 @@
-export default function RestTodosPage() {
+//"use client";
+
+import prisma from "@/lib/prisma";
+import { NewTodo, TodosGrid } from "@/todos";
+import { useEffect } from "react";
+
+export const metadata = {
+  title: "Listado de todos",
+  description: "SEO title",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
+
+  /*useEffect(() => {
+    fetch("/api/todos")
+      .then((resp) => resp.json())
+      .then(console.log);
+  }, []);*/
+
   return (
     <div>
-      <h1>Hello restTodos </h1>
+      <div className="w-full px-3 mx-5 mb-5">
+        <NewTodo />
+      </div>
+      <TodosGrid todos={todos} />
     </div>
   );
 }
---
+++ src/todos/helpers/todos.ts
@@ -0,0 +1,44 @@
+import { Todo } from "@prisma/client";
+
+export const updateTodo = async (
+  id: string,
+  complete: boolean
+): Promise<Todo> => {
+  const body = { complete: complete };
+
+  // Si lo llamo del servidor tengo que poner la URL completa
+  const todo = await fetch(`/api/todos/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  return todo;
+};
+
+export const createTodo = async (description: string): Promise<Todo> => {
+  const body = { description };
+
+  const todo = await fetch(`/api/todos`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  return todo;
+};
+
+export const deleteCompletedTodos = async (): Promise<boolean> => {
+  await fetch(`/api/todos`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  return true;
+};

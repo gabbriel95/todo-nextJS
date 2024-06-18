d1aed72d904ed0975fbed66a19616468b07631dd
Gabriel <juanmendez0013@gmail.com>
2024-06-18 11:56:09 -0300

    feat: credentials provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,43 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
 import google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./lib/prisma"; // Asegúrate de que prisma es una instancia de PrismaClient
+import { signInEmailPassword } from "./auth/actions/auth-actions";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [github, google],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    github,
+    google,
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (
+          !credentials ||
+          typeof credentials.email !== "string" ||
+          typeof credentials.password !== "string"
+        ) {
+          console.log("ERRORRR");
+          return null;
+        }
+
+        const user = await signInEmailPassword(
+          credentials.email,
+          credentials.password
+        );
+
+        if (user) {
+          console.log("TODO OK EN RETORNO DE USUARIO");
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 });
